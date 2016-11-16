46589da0d7b045501bb23a8dfe38205e887928d9
vinau <andres.vinau@gmail.com>
2016-11-16 09:57:44 +0100

    Avanzando con el ejercicio 10

--- Ejercicio01/src/Ejercicio_10.java
+++ Ejercicio01/src/Ejercicio_10.java
@@ -8,37 +8,49 @@ public class Ejercicio_10 {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner (System.in);
 		
-		int numero,divisor=2;
+		int numero, divisor=2;
 		boolean esPrimo=true;
 		
-		System.out.println ("Ingrese un número");
-		numero= teclado.nextInt ();
+		System.out.println ("Introduzca un número:");
+		numero = teclado.nextInt();
 		
 		numero++;
-				
+		
 		while (esPrimo && divisor <numero){
 			
 			esPrimo=numero%divisor !=0;
 			divisor++;
+			
 		}
 		
-		while(esPrimo==false){
-			
-			numero++;
-			
-			while (esPrimo && divisor <numero){
-				
-				esPrimo=numero%divisor !=0;
-				divisor++;
-			}
-		}
-		
-			if (esPrimo==true){
+		if (esPrimo==true){
 			
 			System.out.println("el número"+numero+"es primo");
 			
 		}
 		
+		if (esPrimo==false){
+			
+			//Aqui hay que repetir el proceso para que siga sumando al valor numero cada vez que esPrimo sea falso.
+			boolean esPrimo2=true;
+			esPrimo2=true;
+			int divisor2=2;
+			
+			while (esPrimo2 && divisor2 <numero){
+				
+				numero++;
+				
+				esPrimo2=numero%divisor2 !=0;
+				divisor2++;
+				
+			}
+			
+			if (esPrimo2==true){
+				
+				System.out.println("el número"+numero+"primo");
+				
+		}
+		
 	}
-	
+}
 }
